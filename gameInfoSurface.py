c9df036ed51cbab0eac07037bf43ca2efebf569d
Eduard Dumitrescul <dumitrescul.eduard@gmail.com>
2022-12-15 09:06:42 +0200

    created basic linearLayout

--- gameInfoSurface.py
+++ gameInfoSurface.py
@@ -1,15 +1,27 @@
-import pygame.surface
+from pygame import Surface
+
+import linearLayout
+from linearLayout import LinearLayout
 
 WHITE = (255, 255, 255)
 DEBUG = (100, 255, 255)
+DEBUG2 = (200, 255, 255)
 
-WHITE = DEBUG
 
-
-class GameInfoSurface(pygame.surface.Surface):
+class GameInfoSurface(Surface):
     def __init__(self, width, height, game_data):
         super().__init__([width, height])
 
         self.game_data = game_data
 
         self.fill(WHITE)
+
+        linear_layout = LinearLayout([100, 100], linearLayout.HORIZONTAL)
+        s1 = Surface([20, 20])
+        s1.fill(DEBUG)
+        s2 = Surface([20, 20])
+        s2.fill(DEBUG2)
+        linear_layout.add_surface(s1)
+        linear_layout.add_surface(s2)
+
+        self.blit(linear_layout, [0, 0])
---
+++ linearLayout.py
@@ -0,0 +1,37 @@
+from pygame import Surface
+
+VERTICAL = 0
+HORIZONTAL = 1
+
+
+class LinearLayout(Surface):
+    def __init__(self, size, orientation):
+        super().__init__(size)
+
+        self.orientation = orientation
+        self.padding_begin = 0
+        self.padding_end = 0
+        self.padding_top = 0
+        self.padding_bottom = 0
+        self.surface_list = []
+
+    def set_padding(self, begin, top, end, bottom):
+        self.padding_begin = begin
+        self.padding_top = top
+        self.padding_end = end
+        self.padding_bottom = bottom
+
+    def add_surface(self, surface):
+        self.surface_list.append(surface)
+        self.update()
+
+    def update(self):
+        x, y = self.padding_begin, self.padding_top
+        for surface in self.surface_list:
+            self.blit(surface, [x, y])
+            if self.orientation == VERTICAL:
+                y += surface.get_rect().height
+            else:
+                x += surface.get_rect().width
+
+
